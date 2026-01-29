687d8397574fdfc1f4102dfeda1199882ee8e688
Harshavardhanreddy-m <harshavardhanr272@gmail.com>
2026-01-29 13:57:38 +0530

    actions

--- 07-installation.sh
+++ 07-installation.sh
@@ -9,3 +9,28 @@ fi
 
 echo "Installing  Nginx"
 dnf install nginx  -y
+
+if [ $USERID -ne 0 ]; then
+    echo "Installing  Nginx ... FALIURE"
+    exit 1
+else
+    echo "Installing  Nginx ... SUCCESS"
+fi
+
+dnf install nginx  -y
+
+if [ $USERID -ne 0 ]; then
+    echo "Installing  MySQL ... FALIURE"
+    exit 1
+else
+    echo "Installing  MySQL ... SUCCESS"
+fi
+
+dnf install nodejs  -y
+
+if [ $USERID -ne 0 ]; then
+    echo "Installing  nodejs ... FALIURE"
+    exit 1
+else
+    echo "Installing  nodejs ... SUCCESS"
+fi
