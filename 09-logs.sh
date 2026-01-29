9b6d8cc19334a730f78f18dcce09be1a5574fda9
Harshavardhanreddy-m <harshavardhanr272@gmail.com>
2026-01-29 14:39:02 +0530

    actions

--- 09-logs.sh
+++ 09-logs.sh
@@ -5,7 +5,7 @@ LOGD-FOLDER="/var/log/shell-script"
 LOGS-FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "Please run this script with root user access "
+    echo "Please run this script with root user access"  | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-         echo "$2 ... FALIURE"
+         echo "$2 ... FALIURE"  | tee -a $LOGS_FILE
          exit 1
 else
-         echo "$2 ... SUCCESS"
+         echo "$2 ... SUCCESS"  | tee -a $LOGS_FILE
 fi
 }
 
